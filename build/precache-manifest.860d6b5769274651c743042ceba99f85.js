57cb44d7852f882f3226467bf94bbd411e996fba
Austris <austris.cirulnieks@gmail.com>
2020-10-09 01:03:27 +0300

    Upadtes build folder

---
+++ build/precache-manifest.860d6b5769274651c743042ceba99f85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db99bae30cdecd2d6846cfb1d0491a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8977cc4f9e6ff0bf0b2a",
+    "url": "/static/css/main.53e2993a.chunk.css"
+  },
+  {
+    "revision": "0d12b5077a6689d9e729",
+    "url": "/static/js/2.be562cb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.be562cb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8977cc4f9e6ff0bf0b2a",
+    "url": "/static/js/main.4b14e297.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
