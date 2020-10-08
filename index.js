1d980abe8f5c6c7a94d0b4fd006c524651ce2778
Austris <austris.cirulnieks@gmail.com>
2020-10-09 00:40:31 +0300

    adds build folder

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors')
+const cors = require('cors');
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
+app.use(express.static('build'));
 app.use(
   morgan(function (tokens, req, res) {
     let person = null;
