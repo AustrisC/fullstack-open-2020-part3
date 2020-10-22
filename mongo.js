17ac6f470cd7fff930d923ddd7fd66488cf7888c
Austris <austris.cirulnieks@gmail.com>
2020-10-22 19:11:11 +0300

    Finishes task 3

---
+++ mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password: node mongo.js <password>');
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const DB_URL = `mongodb+srv://fullstack:${password}@cluster0.brrsg.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+if (process.argv.length == 3) {
+  console.log('phonebook:');
+  Person.find({}).then((result) => {
+    result.forEach((p) => {
+      console.log(`${p.name} ${p.number}`);
+    });
+    mongoose.connection.close();
+  });
+} else if (process.argv.length == 5) {
+  const name = process.argv[3];
+  const number = process.argv[4];
+
+  const person = new Person({
+    name,
+    number,
+  });
+
+  person.save().then((result) => {
+    console.log(`Added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
---
+++ person.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const DB_URL = process.env.MONGO_URL;
+
+mongoose
+  .connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then((_) => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+personSchema.plugin(uniqueValidator);
+
+personSchema.set('toJSON', {
+  transform: (_, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('Person', personSchema);
